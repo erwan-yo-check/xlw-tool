fc4d035b1b56b77c5167a06a012020e594ca9f09
macPro_check_erwan <1076360205@qq.com>
2023-06-09 13:40:42 +0800

    feat: jackson 结构转换新增方式

--- tool-util/src/main/java/org/xlw/common/util/JacksonUtils.java
+++ tool-util/src/main/java/org/xlw/common/util/JacksonUtils.java
@@ -2,6 +2,9 @@ package org.xlw.common.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
 
 /**
  * Description: json处理工具-important，尽量不用fastJson，有很多bug
@@ -9,6 +12,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * Email: 1076360205@qq.com
  * Date: 2023/5/31 17:29
  */
+@Slf4j
 public class JacksonUtils {
 
     /**
@@ -20,10 +24,52 @@ public class JacksonUtils {
      * @throws
      * @since
     */
-    public static <T>T parseJson(Class<T> clazz, String json) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        //json字符串转成对象
-        T res = mapper.readValue(json, clazz);
-        return res;
+    public static <T>T parseJson(Class<T> clazz, String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            //json字符串转成对象
+            return mapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            log.error("jacksonUtil error: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @desc jsonObj转化为json字符串
+     *
+     * @date 2023/6/9 13:30
+     * @param jsonObject 结构体
+     * @return java.lang.String
+     * @throws
+     * @since
+    */
+    public static String parseJsonObj(Object jsonObject) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(jsonObject);
+        } catch (JsonProcessingException e) {
+            log.error("jacksonUtil error: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @desc map转JsonString
+     *
+     * @date 2023/6/9 13:40
+     * @param map hashMap
+     * @return java.lang.String
+     * @throws
+     * @since
+    */
+    public static String parseMap(HashMap<String,Object> map) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            log.error("jacksonUtil error: ", e);
+            throw new RuntimeException(e);
+        }
     }
 }
