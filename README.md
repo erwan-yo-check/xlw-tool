21e80631f7372b476bb8d55b293e87e9f7d4773d
macPro_check_erwan <1076360205@qq.com>
2023-06-09 11:23:15 +0800

    feat: nacos quick start

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # xlw-tool
+
+提供springboot-nacos-dubbo的模式下开发基础工具
+
+包含：工具类
+注解类
--- tool-nacos/src/main/java/org/xlw/common/Main.java
+++ tool-nacos/src/main/java/org/xlw/common/Main.java
@@ -1,24 +1,51 @@
 package org.xlw.common;
 
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import org.xlw.common.util.JacksonUtils;
+
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
 /**
  * Description: check_yo
  * Author: erwan_check
  * Email: 1076360205@qq.com
  * Date: 2023/6/8 09:42
- */// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+ */
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            String serverAddr = "{serverAddr}";
+            String dataId = "{dataId}";
+            String group = "{group}";
+            Properties properties = new Properties();
+            properties.put("serverAddr", serverAddr);
+            ConfigService configService = NacosFactory.createConfigService(properties);
+            String content = configService.getConfig(dataId, group, 5000);
+            // json转对象，然后优化修改
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+            System.out.println(content);
+            configService.addListener(dataId, group, new Listener() {
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    // 做一些更新处理
+                    System.out.println("recieve1:" + configInfo);
+                }
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+            });
+
+
+        } catch (NacosException e) {
+            throw new RuntimeException(e);
         }
     }
 }
