d2efff1713ca9b9a7385ec56c7e7e2687b6a5cb8
macPro_check_erwan <1076360205@qq.com>
2023-06-20 15:25:42 +0800

    feat: 分页返回和返回值能力

--- tool-util/src/main/java/org/xlw/common/model/requests/PageBaseRequest.java
+++ tool-util/src/main/java/org/xlw/common/model/requests/PageBaseRequest.java
@@ -1,10 +1,26 @@
 package org.xlw.common.model.requests;
 
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * Description: 分页查询基础请求
  * Author: erwan_check
  * Email: 1076360205@qq.com
  * Date: 2023/6/20 14:13
  */
-public class PageBaseRequest {
+@Data
+public class PageBaseRequest implements Serializable {
+
+    private static final long serialVersionUID = 5459987233657926259L;
+    /**
+     * 当前页数
+     */
+    private Integer currentPage;
+
+    /**
+     * 一页最多有多少条数据
+     */
+    private Integer pageSize;
 }
